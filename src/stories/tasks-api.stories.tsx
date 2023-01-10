e6cca4adf815be11ee40711b2511245d848b8605
Ilya <koselevicila@gmail.com>
2023-01-10 15:46:42 +0300

    Delete-input-button-task

--- src/stories/tasks-api.stories.tsx
+++ src/stories/tasks-api.stories.tsx
@@ -17,15 +17,28 @@ export const GetTask = () => {
 }
 export const DeleteTask = () => {
     const [state, setState] = useState<any>(null)
-    useEffect(() => {
-        const todolistId = ''
+    const [taskId, setTaskId] = useState<any>(null)
+    const [todolistId, setTodolistId] = useState<any>(null)
+    const deleteTask = () => {
+        const todolistId = '62a788fb-84e8-43be-af57-16334fb7e8d7'
         const taskId = ''
         tasksApi.deleteTask(todolistId, taskId)
             .then((res) => {
                 setState(res.data)
             })
-    }, [])
-    return <div> {JSON.stringify(state)}</div>
+    }
+    return <div>
+        {JSON.stringify(state)}
+        <div>
+            <input placeholder={'todolistId'} value={todolistId} onChange={(e) => {
+                setTodolistId(e.currentTarget.value)
+            }}/>
+            <input placeholder={'taskId'} value={taskId} onChange={(e) => {
+                setTaskId(e.currentTarget.value)
+            }}/>
+            <button onChange={deleteTask}>deleteTask</button>
+        </div>
+    </div>
 }
 export const PutTask = () => {
     const [state, setState] = useState<any>(null)
